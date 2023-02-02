12edd4c3b736bcae52215a47a116c12e849126b9
saurabh27pal <saurabh27pal1999@gmail.com>
2023-02-02 20:16:52 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function letterCombinations(input_digit) {
+// const arr = [0, 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+//0=>0 1=>1 2=>abc 3=>def 4=>ghi 5=>jkl 6=>mno 7=>pqrs 8=>tuv 9=>wxyz
+
+function letterCombinations(digits) {
   //Complete the function
+  if (digits == null || digits.length === 0) return [];
+
+  const map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  const res = [];
+  const go = (i, s) => {
+    if (i === digits.length) {
+      res.push(s);
+      return;
+    }
+
+    for (const c of map[digits[i]]) {
+      go(i + 1, s + c);
+    }
+  };
+
+  go(0, '');
+  return res;
 }
 
+//letterCombinations(23);
+       
 module.exports = letterCombinations;
